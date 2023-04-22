85ff6b0688a468becb5d98ad9b09f86f978a7821
Richard <68445202+richardYoutube@users.noreply.github.com>
2023-04-22 14:38:50 -0400

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -6,7 +6,7 @@ using StringTools;
 
 class CoolUtil
 {
-	public static var difficultyArray:Array<String> = ['Easy', "Normal", "Hard"];
+	public static var difficultyArray:Array<String> = ['Infinitely Easy', "Extremely Easy", "Very Easy", "Easy"];
 
 	public static var daPixelZoom:Float = 6;
 
